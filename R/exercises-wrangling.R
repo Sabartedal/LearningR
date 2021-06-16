0c32e635a973c28f469aab557abdd9b12a7993f4
Sabina Stoffer Hjorth Andersen <sabina@nexs.ku.dk>
2021-06-16 10:19:50 +0200

    Small data

--- R/exercises-wrangling.R
+++ R/exercises-wrangling.R
@@ -22,3 +22,26 @@ select(NHANES, Age, BMI)
 
 select(NHANES, -Age)
 
+# All columns starting with "BP"
+
+select(NHANES, starts_with("BP"))
+
+# All columns ending with "Day"
+
+select(NHANES, ends_with("Day"))
+
+# All columns containing "Age"
+
+select(NHANES, contains("Age"))
+
+?select_helpers
+
+# Save the selected columns as a new data frame
+
+nhanes_small <- select(NHANES, Age, Gender, Height,
+                       Weight, BMI, Diabetes, DiabetesAge,
+                       PhysActiveDays, PhysActive, TotChol,
+                       BPSysAve, BPDiaAve, SmokeNow, Poverty)
+nhanes_small
+
+
